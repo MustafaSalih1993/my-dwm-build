cf8ab48cd295cb8a723ad171e9c2cee877c0e408
mustafa salih <mustafasalih1991@gmail.com>
2020-11-15 22:49:05 +0300

    updated

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Font Awesome 5 Free:size=10" };
+static const char *fonts[]          = { "Font Awesome 5 Free:size=11" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -41,11 +41,11 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 #include "fibonacci.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    { "[@]",      spiral  },
     { "[]=",      tile },    /* first entry is default */
     { "><>",      NULL },    /* no layout function means floating behavior */
     { "[M]",      monocle },
     { "[\\]",     dwindle  },
+    { "[@]",      spiral  },
 };
 
 /* key definitions */
