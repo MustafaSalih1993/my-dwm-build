dd5d90b3411e392664d79e356220450e3cc101c4
mustafa salih <mustafasalih1991@gmail.com>
2020-12-03 04:27:46 +0300

    updated configs

--- config.h
+++ config.h
@@ -28,7 +28,6 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,                     1,           -1 },
     { "Firefox",  NULL,       NULL,       1 << 2,	         0,           -1 },
     { "Code",     NULL,	      NULL,	  2,    	         0,           -1 },
 };
